7054460d8de006fad00e712221b62602cd6f62a3
dpurser-dev <david.purser@kellogg.northwestern.edu>
2022-04-15 23:59:25 +0000

    Update

--- kmdb.sql
+++ kmdb.sql
@@ -100,14 +100,64 @@
 .headers off
 
 -- Drop existing tables, so you'll start fresh each time this script is run.
--- TODO!
+DROP TABLE IF EXISTS movies;
+DROP TABLE IF EXISTS actors;
+DROP TABLE IF EXISTS actors_movies;
+DROP TABLE IF EXISTS studios;
 
 -- Create new tables, according to your domain model
--- TODO!
+CREATE TABLE movies(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT, 
+    year_released INTEGER, 
+    MPAA_rating TEXT, 
+    studio_id INTEGER
+);
+
+CREATE TABLE actors(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT,
+    last_name TEXT
+);
+
+CREATE TABLE actors_movies(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    actor_id INTEGER,
+    role TEXT,
+    movie_id INTEGER
+);
+
+CREATE TABLE studios(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT
+);
 
 -- Insert data into your database that reflects the sample data shown above
 -- Use hard-coded foreign key IDs when necessary
--- TODO!
+INSERT INTO movies (title, year_released, MPAA_rating, studio_id)
+VALUES ('Batman Begins', 2005, 'PG-13', 'Warner Bros.'),
+('The Dark Knight', 2008, 'PG-13', 'Warner Bros.'),
+('The Dark Knight Rises', 2012, 'PG-13', 'Warner Bros.');
+
+INSERT INTO actors(first_name, last_name)
+VALUES('Christian', 'Bale'),
+('Michael', 'Caine'),
+('Liam', 'Neeson'),
+('Katie', 'Holmes'),
+('Gary', 'Oldman'),
+('Christian', 'Bale'),
+('Heath', 'Ledger'),
+('Aaron', 'Eckhart'),
+('Michael', 'Caine'),
+('Maggie', 'Gyllenhaal'),
+('Christian', 'Bale'),
+('Gary', 'Oldman'),
+('Tom', 'Hardy'),
+('Joseph', 'Gordon-Levitt'),
+('Anne',' Hathaway');
+
+INSERT INTO actos_movies(actor_id, role, movie_id)
+VALUES()
 
 -- Prints a header for the movies output
 .print "Movies"
